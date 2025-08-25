a5500cb25b24356d6e256053ef41b3b846d739c2
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2025-08-25 23:54:52 +1200

    install dependen cies

--- transfersecure-server/dist/index.js
+++ transfersecure-server/dist/index.js
@@ -1,8 +1,7 @@
-import fastify, {} from 'fastify';
-import * as client from 'openid-client';
+import fastify from 'fastify';
 import fastifySession from '@fastify/session';
 import fastifyCookie from '@fastify/cookie';
-import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
+import { Amplify } from 'aws-amplify';
 import 'dotenv/config';
 const server = fastify();
 server.register(fastifyCookie);
@@ -12,16 +11,20 @@ server.register(fastifySession, {
         secure: false,
     },
 });
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolClientId: process.env.CLIENT_ID,
+            userPoolId: process.env.USER_POOL_ID,
+            signUpVerificationMethod: 'code',
+        },
+    },
+});
 server.get('/ping', async (request, reply) => {
     return 'pong\n';
 });
-var poolData = {
-    UserPoolId: process.env.USER_POOL_ID, // Your user pool id here
-    ClientId: process.env.CLIENT_ID, // Your client id here
-};
-var userPool = new CognitoUserPool(poolData);
-const url = new URL(process.env.SERVER);
-let config = await client.discovery(url, process.env.CLIENT_ID, process.env.CLIENT_SECRET);
+server.post('/register', async (request, reply) => { });
+server.post('/login', async (request, reply) => { });
 server.listen({ port: 8080 }, (err, address) => {
     if (err) {
         console.error(err);
@@ -29,56 +32,5 @@ server.listen({ port: 8080 }, (err, address) => {
     }
     console.log(`Server listening at ${address}`);
     console.log(process.env.SERVER);
-});
-let isAuthentcated = false;
-const authHandler = (request, reply) => {
-    if (request.session) {
-        // Session object exists, a session is likely active
-        // console.log('Session is set:', request.session);
-        isAuthentcated = true;
-    }
-    reply.send(isAuthentcated);
-};
-server.get('/', { preHandler: authHandler }, (request, reply) => {
-    // if (isAuthentcated == true){
-    //   // reply.redirect("")
-    //   console.log("Authentitcated")
-    //   // return isAuthentcated
-    // }else{
-    //   console.log("Sad")
-    // }
-    reply.send(isAuthentcated);
-});
-server.post('/register', async (request, reply) => {
-    console.log(request);
-    const { email, password } = request.body;
-    userPool.signUp(email, password, [], [], (err, result) => {
-        if (err) {
-            console.log(err);
-            return reply.status(500).send('Error registering user');
-        }
-        const cognitoUser = result.user;
-        console.log('User registered successfully:', cognitoUser.getUsername());
-        reply.send('User registered successfully');
-    });
-});
-server.post('/login', async (request, reply) => {
-    // var attributeList = [];
-    // const { email, password } = request.body as loginData;
-    // let emailVariable = {
-    //     Name: 'email',
-    //     Value: email,
-    // };
-    // var attributeEmail = new CognitoUserAttribute(emailVariable);
-    // attributeList.push(attributeEmail);
-    // userPool.signUp(email, password, attributeList, [], function (err, result) {
-    //     if (err) {
-    //         console.log(err);
-    //         return;
-    //     }
-    //     var cognitoUser = result!.user;
-    //     console.log('user name is ' + cognitoUser.getUsername());
-    // });
-    const { email, password } = request.body;
 });
 //# sourceMappingURL=index.js.map
