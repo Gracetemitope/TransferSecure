389a3f5a71d0234759d8087a0cdf0465017d4f90
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2025-08-29 19:36:40 +1200

    Install and configure tailwind css

--- transfersecure-client/src/App.js
+++ transfersecure-client/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <h1>Now you can Transfer Securely</h1>
+       <h1></h1>
+        <h1 className="text-6xl font-bold text-red-600 p-4">Now you can Transfer Securely</h1>
+
     </div>
   );
 }
